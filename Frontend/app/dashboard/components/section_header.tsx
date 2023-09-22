72ca7fb0a9a74f6dd535e460a6c5c73a5118233c
hoanggiapvu79116 <hoanggiapvuvhg@gmail.com>
2023-09-22 20:43:48 +1000

    fixed sectionHeader flashing during redirection

--- Frontend/app/dashboard/components/section_header.tsx
+++ Frontend/app/dashboard/components/section_header.tsx
@@ -11,21 +11,30 @@ export default function SectionHeader({
     sectionTabs
 }: { 
     sectionTabs: SectionTabData[]
-}): JSX.Element { 
+}): JSX.Element | null { 
     const pathName = usePathname(); 
     const userInfo = getUserInfo(); 
 
-    return (
-            // {/* If length is not converted to String -> lead to bug, I do not reall know why */}
-        <nav className={`h-10 w-full grid grid-cols-12 items-center bg-gray-400`}>            
-            {
-                sectionTabs.map((sectionTab, index) => 
-                    <SectionTab 
-                        key={index}
-                        selected={pathName === sectionTab.href} 
-                        sectionTab={sectionTab}/>
-                )
-            }
-        </nav>
-    )
+    // Find the current section based on the current URL. 
+    const currentSectionTab = sectionTabs.find(sectionTab => sectionTab.href === pathName); 
+
+    // If the current URL does not correspond to any sectionTab in sectionHeader, do not render this sectionHeader
+    // Otherwise, render the sectionHeader and highlight the corresponding sectionTab.   
+    if(currentSectionTab === undefined) {
+        return null; 
+    }
+    else { 
+        return (
+            <div className={`h-10 w-full grid grid-cols-12 items-center bg-gray-400`}>
+                {
+                    sectionTabs.map((sectionTab, index) => 
+                        <SectionTab 
+                            key={index}
+                            selected={pathName === sectionTab.href} 
+                            sectionTab={sectionTab}/>
+                    )
+                }
+            </div>
+        ); 
+    }
 }
