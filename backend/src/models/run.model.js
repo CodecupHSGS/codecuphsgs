4bf1077de68821692ca060ceaaff8acf1edc5dca
hoanggiapvu79116 <hoanggiapvuvhg@gmail.com>
2023-09-23 16:30:11 +1000

    Updated run schema

--- backend/src/models/run.model.js
+++ backend/src/models/run.model.js
@@ -3,7 +3,9 @@ import { Schema, model } from "mongoose";
 const runSchema = { 
     id: {type: Number, required: true}, 
     contestId: {type: Number, required: true}, 
-    official: {type: Boolean, required: true}, 
+    includeUnofficial: {type: Boolean, required: true}, 
+    startDate: {type: Date, required: true}, 
+    endDate: {type: Date}, 
 	finishedJudging: { type: Boolean, default: false}, 
 	cache: {type: String, default: ""},
 	results: {type: String, default: ""}
